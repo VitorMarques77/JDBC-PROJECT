f8ea43e8c524dba18b26d0401d7cde5dec214365
Vitor Marques Costa <vmarquec@everis.com>
2020-01-20 10:26:14 -0300

    Projeto finalizado

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,6 @@
 package application;
 
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -10,10 +11,31 @@ public class Program2 {
 		
 		DepartmentDao dd = DaoFactory.createDepartmentDado();
 		Department obj = new Department(null, "Music");
-		dd.insert(obj);
 		System.out.println("=== TEST 1: department insert ===");
+		dd.insert(obj);
 		System.out.println("Inserted! New Id = "+obj.getId());
 		
+		
+		System.out.println();
+		System.out.println("=== TEST 2: department findById ===");
+		Department dp2 = dd.findById(3);
+		System.out.println(dp2);
+		
+		System.out.println();
+		System.out.println("=== TEST 3: department update ===");
+		Department dp3 = dd.findById(1);
+		dp3.setName("Culture");
+		
+		
+		System.out.println();
+		System.out.println("=== TEST 4: department deleteById ===");
+		dd.deleteById(7);
+		
+		System.out.println();
+		System.out.println("=== TEST 5: department findAll ===");
+		dd.findAll().forEach(System.out::println);;
+		
+		DB.closeConnection();
 		
 	}
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -6,16 +6,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import db.DB;
 import db.DbException;
 import model.dao.DepartmentDao;
-import model.dao.SellerDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
 
@@ -62,24 +58,97 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public void update(Department obj) {
+		PreparedStatement st = null;
+		try {
+		st = conn.prepareStatement(
+				"UPDATE department "
+				+"SET Name= ? "
+				+"WHERE Id = ? ");
 		
-		
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			st.executeUpdate();
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected == 0) {
+				throw new DbException("Delete Failed! Invalid Id");
+			}
+			else {
+				System.out.println("Done! Rows Affected: "+rowsAffected);
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public Department findById(Integer id) {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				Department obj = new Department(rs.getInt(1), rs.getString(2));
+				return obj;
+			}
+			else {
+				return null;
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public List<Department> findAll() {
-		return null;
+		List<Department> list = new ArrayList<>();
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM department ORDER BY Name");
+			rs = st.executeQuery();
+			while(rs.next()) {
+				list.add(new Department(rs.getInt(1), rs.getString(2)));
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 	
 }
